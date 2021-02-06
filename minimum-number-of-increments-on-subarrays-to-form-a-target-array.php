62032357639bf38168f336549c0615e7a7317549
Rishi <rishiagrawal61@gmail.com>
2021-02-06 21:40:24 +0530

    new files updated

---
+++ minimum-number-of-increments-on-subarrays-to-form-a-target-array.php
@@ -0,0 +1,14 @@
+class Solution {
+
+    /**
+     * @param Integer[] $target
+     * @return Integer
+     */
+    function minNumberOperations($target) {
+        $a=$target[0];
+        for($i=1;$i<count($target);$i++){
+            $a+=max(0,$target[$i]-$target[$i-1]);
+        }
+        return $a;
+    }
+}
